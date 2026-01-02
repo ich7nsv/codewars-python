74e8046febd4c6ab0bffb2c26da0effa5d7a609a
i-chernousov <iche7n@gmail.com>
2026-01-02 10:30:21 +0300

    Create double_integer.py

---
+++ 8kyu/double_integer.py
@@ -0,0 +1,11 @@
+"""
+Code as fast as you can! You need to double the integer and return it.
+"""
+
+
+def double_integer(i: int) -> int:
+    """Функция дублирует поступившее целочисленное число"""
+    return i * 2
+
+if __name__ == "__main__":
+    print(double_integer(2))
